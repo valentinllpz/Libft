32e1eb155f4f58b171acbe1820ee0d8488028326
Valentin LUGAND LOPEZ <vlugand-@e2r1p12.42.fr>
2019-11-18 14:36:18 +0100

    minor fix all OK

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: vlugand- <vlugand-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 12:40:48 by vlugand-          #+#    #+#             */
-/*   Updated: 2019/11/14 20:08:56 by vlugand-         ###   ########.fr       */
+/*   Updated: 2019/11/18 14:34:59 by vlugand-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char		*dst;
 	size_t		i;
+	size_t		size;
 
 	if (!s)
 		return (NULL);
 	if (start + len > ft_strlen(s))
 		return (ft_strdup(""));
+	size = ft_strlen(s + start);
+	if (len > size)
+		len = size;
 	if (!(dst = (char *)malloc(sizeof(char) * len + 1)))
 		return (NULL);
 	i = 0;
